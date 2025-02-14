7a1e5b2cb59e346d6e18b574a7dc38aaae5a7773
Jo0o0ol <klim0000019@gmail.com>
2025-02-14 15:34:23 +0300

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Rectangle
+{
+    private double width;
+    private double height;
+
+    public Rectangle()
+    {
+        width = 0;
+        height = 0;
+        Console.WriteLine("Конструктор по умолчанию вызван");
+    }
+
+    public Rectangle(double side)
+    {
+        width = side;
+        height = side;
+        Console.WriteLine("Конструктор для квадрата вызван");
+    }
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+        Console.WriteLine("Конструктор с шириной и высотой вызван");
+    }
+
+    ~Rectangle()
+    {
+        Console.WriteLine("Деструктор вызван");
+    }
+
+    public double CalculateArea()
+    {
+        return width * height;
+    }
+
+    public void DisplayArea()
+    {
+        Console.WriteLine($"Площадь прямоугольника: {CalculateArea()}");
+    }
+    public static void Main(string[] args)
+    {
+        Rectangle rect1 = new Rectangle();
+        rect1.DisplayArea();
+
+        Rectangle rect2 = new Rectangle(5);
+        rect2.DisplayArea();
+
+        Rectangle rect3 = new Rectangle(4, 6);
+        rect3.DisplayArea();
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+    }
+}
